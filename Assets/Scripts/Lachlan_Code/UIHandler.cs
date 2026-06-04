01c22b59b14888c0b70b58fe9ae390ba01317c7b
butts28 <butlerlachlan740@gmail.com>
2026-06-04 10:07:41 +0800

    Modification: UIHandler Script edited and Exit function added
    In this commit I have created new code in my UI Handler script that allows the player to exit to desktop from the main menu.

--- Assets/Scripts/Lachlan_Code/UIHandler.cs
+++ Assets/Scripts/Lachlan_Code/UIHandler.cs
@@ -1,29 +1,104 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class UIHandler : MonoBehaviour
+namespace Lachlan
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class UIHandler : MonoBehaviour
     {
-        
-    }
-    public void HighscoreCounter()
-    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    }
-    public void ScoreCounter()
-    {
-        
-    }
+        }
+        public void HighscoreCounter()
+        {
+
+        }
+        public void ScoreCounter()
+        {
+
+        }
+        public void UpdateScore()
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+        #region Singleton
+        public static UIHandler instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != null && instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        #endregion
+
+        #region Variables
+        public GameState currentState = GameState.PreGame;
+        public float globalSpeed = 1.0f;
+        public GameObject[] menuPanels = new GameObject[3];
+
+        public int score = 0;
+        public Text scoreText;
+        #endregion
+
+        void SetMenuPanel()
+        {
+            for (int i = 0; i < menuPanels.Length; i++)
+            {
+                menuPanels[i].SetActive(false);
+            }
+            menuPanels[(int)currentState].SetActive(true);
+        }
+        public void SetState(GameState newState)
+        {
+            currentState = newState;
+
+            switch (currentState)
+            {
+                case GameState.PreGame:
+                    score = 0;
+                    globalSpeed = 0;
+                    break;
+                case GameState.Game:
+                    globalSpeed = 1.0f;
+                    break;
+                case GameState.PostGame:
+                    globalSpeed = 0;
+                    break;
+            }
+            SetMenuPanel();
+        }
+        public void RestartGame()
+        {
+            SetState(GameState.PreGame);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
 
-    public void ExitToDesktop()
-    {
+        private void OnAwake()
+        {
+            SetState(GameState.PreGame);
+        }
+        public void ExitToDesktop()
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #endif
-        Application.Quit();
+            Application.Quit();
+        }
+    }
+    public enum GameState
+    {
+        PreGame,
+        Game,
+        PostGame,
     }
 }
